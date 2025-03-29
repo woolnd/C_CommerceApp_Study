8031f4970eaf4299f41a2153430cc71888dd52a9
Riu <wodnd0418@naver.com>
2025-03-30 06:46:19 +0900

    [DESIGN] 디자인 리소스(이미지, 색상, 폰트) 컴포넌트화

---
+++ Cproject/DesignSource/CPColor/CPColor.swift
@@ -0,0 +1,27 @@
+//
+//  CPColor.swift
+//  Cproject
+//
+//  Created by wodnd on 3/30/25.
+//
+
+import UIKit
+
+enum CPColor {
+    static var bk: UIColor = UIColor(named: "bk")!
+    static var wh: UIColor = UIColor(named: "wh")!
+    static var coral: UIColor = UIColor(named: "coral")!
+    static var yellow: UIColor = UIColor(named: "yellow")!
+    static var keyColorRed: UIColor = UIColor(named: "key-color-red")!
+    static var keyColorRed2: UIColor = UIColor(named: "key-color-red-2")!
+    static var keyColorBlue: UIColor = UIColor(named: "key-color-blue")!
+    static var keyColorBlueTrans: UIColor = UIColor(named: "key-color-blue-trans")!
+    static var darkGreen: UIColor = UIColor(named: "dark-green")!
+    static var gray1: UIColor = UIColor(named: "gray-1")!
+    static var gray2: UIColor = UIColor(named: "gray-2")!
+    static var gray3: UIColor = UIColor(named: "gray-3")!
+    static var gray4: UIColor = UIColor(named: "gray-4")!
+    static var gray5: UIColor = UIColor(named: "gray-5")!
+    static var gray6: UIColor = UIColor(named: "gray-6")!
+    static var gray7: UIColor = UIColor(named: "gray-7")!
+}
---
+++ Cproject/DesignSource/CPFont.swift
@@ -0,0 +1,28 @@
+//
+//  CPFont.swift
+//  Cproject
+//
+//  Created by wodnd on 3/30/25.
+//
+
+import UIKit
+
+enum CPFont{
+    static var r10: UIFont = .systemFont(ofSize: 10, weight: .regular)
+    
+    static var r12: UIFont = .systemFont(ofSize: 12, weight: .regular)
+    static var m12: UIFont = .systemFont(ofSize: 12, weight: .medium)
+    static var b12: UIFont = .systemFont(ofSize: 12, weight: .bold)
+    
+    static var r14: UIFont = .systemFont(ofSize: 14, weight: .regular)
+    static var b14: UIFont = .systemFont(ofSize: 14, weight: .bold)
+    
+    static var m16: UIFont = .systemFont(ofSize: 16, weight: .medium)
+    static var b16: UIFont = .systemFont(ofSize: 16, weight: .bold)
+    
+    static var l17: UIFont = .systemFont(ofSize: 17, weight: .light)
+    static var m17: UIFont = .systemFont(ofSize: 17, weight: .medium)
+    static var b17: UIFont = .systemFont(ofSize: 17, weight: .bold)
+    
+    static var b20: UIFont = .systemFont(ofSize: 20, weight: .medium)
+}
